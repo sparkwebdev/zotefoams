69db87734ae9b4a6c0656f7faa5c93ec1a274719
Steven Park <info@stevenpark.co.uk>
2025-02-08 21:45:27 +0000

    404 and no content

--- 404.php
+++ 404.php
@@ -13,43 +13,15 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'zotefoams' ); ?></h1>
-			</header><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'zotefoams' ); ?></p>
+			<header class="text-banner cont-m margin-t-70">
+				<h1 class="uppercase grey-text fs-800 fw-extrabold">404</h1>
+				<h2 class="uppercase black-text fs-800 fw-extrabold"><?php esc_html_e( 'Not found', 'zotefoams' ); ?></h2>
+			</header>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'zotefoams' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$zotefoams_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'zotefoams' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$zotefoams_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+			<div class="cont-m margin-t-70 margin-b-70">
+				<p class="grey-text fs-600 fw-semibold"><?php esc_html_e( 'It looks like nothing was found at this location.', 'zotefoams' ); ?></p>
+			</div>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
