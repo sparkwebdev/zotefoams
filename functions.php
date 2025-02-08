66889bf4c1c46bfed8d6f294a989f4e784ab411e
Steven Park <info@stevenpark.co.uk>
2025-02-08 16:39:42 +0000

    Remove some feature - jetpack, sidebar, widgets, customiser, custom header etc

--- functions.php
+++ functions.php
@@ -54,53 +54,6 @@ function zotefoams_setup() {
 			'quick_links_menu' => esc_html__( 'Quick', 'zotefoams' )
 		)
 	);
-
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
-	add_theme_support(
-		'html5',
-		array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-			'style',
-			'script',
-		)
-	);
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'zotefoams_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
 }
 add_action( 'after_setup_theme', 'zotefoams_setup' );
 
@@ -115,26 +68,6 @@ function zotefoams_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'zotefoams_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'zotefoams_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function zotefoams_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'zotefoams' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'zotefoams' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'zotefoams_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
@@ -177,11 +110,6 @@ function enqueue_swiper_assets() {
 add_action('wp_enqueue_scripts', 'enqueue_swiper_assets');
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
@@ -192,18 +120,6 @@ require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/template-functions.php';
 
 /**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
-
  * Functions which enhance the admin screens by hooking into WordPress.
  */
 require get_template_directory() . '/inc/admin.php';
@@ -212,14 +128,3 @@ require get_template_directory() . '/inc/admin.php';
  * Functions which enhance the admin editor screens by hooking into WordPress.
  */
 require get_template_directory() . '/inc/admin-editor.php';
-
-
-/**
- * Hide Gutenberg Block editor.
- */
-add_filter('use_block_editor_for_post', '__return_false');
-
-/**
- * Disable Application Passwords.
- */
-add_filter( 'wp_is_application_passwords_available', '__return_false' );
