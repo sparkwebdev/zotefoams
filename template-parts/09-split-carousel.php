cce3288ffd231f4d7e4627beedff4b28b3f06ea4
tinnes <mail@tinnes.co.uk>
2025-02-11 09:37:51 +0000

    Removing custom animations from 09-split-carousel

--- template-parts/09-split-carousel.php
+++ template-parts/09-split-carousel.php
@@ -32,20 +32,20 @@
 				<div class="swiper-slide">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/values_courage.png" alt="Materials">
 					<div class="swiper-slide-content">
-						<p class="animate__animated fs-100 margin-b-30 grey-text"  data-animation="animate__fadeInLeft" data-animationdelay="1">More values</p>
-						<p class="animate__animated fs-600 fw-bold"  data-animation="animate__fadeInLeft"  data-animationdelay="1">Courage 2</p>
-						<p class="animate__animated fs-600 fw-medium grey-text margin-b-40" data-animation="animate__fadeInLeft" data-animationdelay="1">The courage to take bold action to ensure that we succeed in tackling our challenges and opportunities.</p>
-						<a href="#" class="animate__animated btn black outline" data-animation="animate__fadeInLeft" data-animationdelay="2">Learn More</a>
+						<p class="animate__animated fs-100 margin-b-30 grey-text">More values</p>
+						<p class="animate__animated fs-600 fw-bold">Courage 2</p>
+						<p class="animate__animated fs-600 fw-medium grey-text margin-b-40">The courage to take bold action to ensure that we succeed in tackling our challenges and opportunities.</p>
+						<a href="#" class="animate__animated btn black outline">Learn More</a>
 					</div>
 					<div class="spacer"></div>
 				</div>
 				<div class="swiper-slide">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/values_courage.png" alt="Materials">
 					<div class="swiper-slide-content">
-						<p class="animate__animated fs-100 margin-b-30 grey-text" data-animation="animate__rollIn">Even more values</p>
-						<p class="animate__animated fs-600 fw-bold" data-animation="animate__rollIn">Courage 3</p>
-						<p class="animate__animated fs-600 fw-medium grey-text margin-b-40" data-animation="animate__rollIn">The courage to take bold action to ensure that we succeed in tackling our challenges and opportunities.</p>
-						<a href="#" class="animate__animated btn black outline" data-animation="animate__rollIn">Learn More</a>
+						<p class="animate__animated fs-100 margin-b-30 grey-text">Even more values</p>
+						<p class="animate__animated fs-600 fw-bold">Courage 3</p>
+						<p class="animate__animated fs-600 fw-medium grey-text margin-b-40">The courage to take bold action to ensure that we succeed in tackling our challenges and opportunities.</p>
+						<a href="#" class="animate__animated btn black outline">Learn More</a>
 					</div>
 					<div class="spacer"></div>
 				</div>
@@ -69,41 +69,6 @@
 					clickable: true,
 				},
 			});
-
-
-			swiperSplit.on('slideChange', function (e) {
-				
-				const previousSlide = swiperSplit.slides[e.previousIndex];
-				toggleAnimation(previousSlide, false);
-
-				const currentSlide = swiperSplit.slides[e.activeIndex];
-				toggleAnimation(currentSlide, true);
-			});
-
-
-			function toggleAnimation(parent, on) {
-				
-				let delay = 0;
-				let elements = parent.getElementsByClassName('animate__animated');
-
-				for (let i = 0; i < elements.length; i++) {
-
-					const animation = elements[i].dataset.animation ?? 'animate__fadeInDown';
-					delay = elements[i].dataset.animationdelay ?? (delay === 0 ? 0.3 : delay + 0.7);
-
-					if (on) {
-						elements[i].classList.add('hidden');
-						if (!elements[i].classList.contains(animation)) {
-							elements[i].style.setProperty('--animation-delay', delay + 's');
-							elements[i].style.setProperty('-webkit-animation-delay', delay + 's');
-							elements[i].classList.add(animation);
-						}
-					} else {
-						elements[i].classList.remove(animation);
-					}
-				}
-			}
-
 		});
 	</script>
 
