499ba282d17afb843713e9851be9a36645577401
tinnes <mail@tinnes.co.uk>
2025-02-11 09:08:03 +0000

    Adding in delay for animations

--- template-parts/09-split-carousel.php
+++ template-parts/09-split-carousel.php
@@ -22,30 +22,30 @@
 				<div class="swiper-slide">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/values_courage.png" alt="Materials">
 					<div class="swiper-slide-content">
-						<p class="animate__animated animate__slow fs-100 margin-b-30 grey-text">Our values</p>
-						<p class="animate__animated animate__slow fs-600 fw-bold">Courage 1</p>
-						<p class="animate__animated animate__slow fs-600 fw-medium grey-text margin-b-40">The courage to take bold action to ensure that we succeed in tackling our challenges and opportunities.</p>
-						<a href="#" class="animate__animated animate__slow btn black outline">Learn More</a>
+						<p class="animate__animated fs-100 margin-b-30 grey-text">Our values</p>
+						<p class="animate__animated fs-600 fw-bold">Courage 1</p>
+						<p class="animate__animated fs-600 fw-medium grey-text margin-b-40">The courage to take bold action to ensure that we succeed in tackling our challenges and opportunities.</p>
+						<a href="#" class="animate__animated btn black outline">Learn More</a>
 					</div>
 					<div class="spacer"></div>
 				</div>
 				<div class="swiper-slide">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/values_courage.png" alt="Materials">
 					<div class="swiper-slide-content">
-						<p class="animate__animated animate__slow fs-100 margin-b-30 grey-text"  data-animation="animate__fadeInLeft">More values</p>
-						<p class="animate__animated animate__slow fs-600 fw-bold"  data-animation="animate__fadeInLeft">Courage 2</p>
-						<p class="animate__animated animate__slow fs-600 fw-medium grey-text margin-b-40"  data-animation="animate__fadeInLeft">The courage to take bold action to ensure that we succeed in tackling our challenges and opportunities.</p>
-						<a href="#" class="animate__animated animate__slow btn black outline"  data-animation="animate__fadeInLeft">Learn More</a>
+						<p class="animate__animated fs-100 margin-b-30 grey-text"  data-animation="animate__fadeInLeft">More values</p>
+						<p class="animate__animated fs-600 fw-bold"  data-animation="animate__fadeInLeft">Courage 2</p>
+						<p class="animate__animated fs-600 fw-medium grey-text margin-b-40"  data-animation="animate__fadeInLeft">The courage to take bold action to ensure that we succeed in tackling our challenges and opportunities.</p>
+						<a href="#" class="animate__animated btn black outline"  data-animation="animate__fadeInLeft">Learn More</a>
 					</div>
 					<div class="spacer"></div>
 				</div>
 				<div class="swiper-slide">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/values_courage.png" alt="Materials">
 					<div class="swiper-slide-content">
-						<p class="animate__animated animate__slow fs-100 margin-b-30 grey-text" data-animation="animate__rollIn">Even more values</p>
-						<p class="animate__animated animate__slow fs-600 fw-bold" data-animation="animate__rollIn">Courage 3</p>
-						<p class="animate__animated animate__slow fs-600 fw-medium grey-text margin-b-40" data-animation="animate__rollIn">The courage to take bold action to ensure that we succeed in tackling our challenges and opportunities.</p>
-						<a href="#" class="animate__animated animate__slow btn black outline" data-animation="animate__rollIn">Learn More</a>
+						<p class="animate__animated fs-100 margin-b-30 grey-text" data-animation="animate__rollIn">Even more values</p>
+						<p class="animate__animated fs-600 fw-bold" data-animation="animate__rollIn">Courage 3</p>
+						<p class="animate__animated fs-600 fw-medium grey-text margin-b-40" data-animation="animate__rollIn">The courage to take bold action to ensure that we succeed in tackling our challenges and opportunities.</p>
+						<a href="#" class="animate__animated btn black outline" data-animation="animate__rollIn">Learn More</a>
 					</div>
 					<div class="spacer"></div>
 				</div>
@@ -83,6 +83,7 @@
 
 			function toggleAnimation(parent, on) {
 				let elements = parent.getElementsByClassName('animate__animated');
+				let delay = 0.3;
 				for (let i = 0; i < elements.length; i++) {
 
 					const animation = elements[i].dataset.animation ?? 'animate__fadeInDown';
@@ -90,12 +91,15 @@
 					if (on) {
 						elements[i].classList.add('hidden');
 						if (!elements[i].classList.contains(animation)) {
+							elements[i].style.setProperty('--animation-delay', delay + 's');
+							elements[i].style.setProperty('-webkit-animation-delay', delay + 's');
 							elements[i].classList.add(animation);
 						}
 					} else {
 						elements[i].classList.remove(animation);
-						elements[i].classList.add('hidden');
 					}
+
+					delay += 0.7;
 				}
 			}
 
