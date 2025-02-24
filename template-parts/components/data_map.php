1cbcc706230a188258fcd4deef9dabfced655d92
Steven Park <info@stevenpark.co.uk>
2025-02-24 20:05:59 +0000

    Data Map CMSable

--- template-parts/components/data_map.php
+++ template-parts/components/data_map.php
@@ -1,23 +1,53 @@
+<?php 
+$bg_image = get_sub_field('data_map_bg');
+$map_image = get_sub_field('data_map_map_image');
 
-	<div class="data-map padding-t-b-100 theme-dark" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/data-map-bg.jpg)">
-		<div class="cont-m">
-			<div class="stats">
-				<div class="stat stat-1">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/data-map.png)" />
-					<p class="fs-700 fw-semibold margin-b-10">120</p>
-					<p class="fs-200 fw-semibold">Graduate positions in 9 global locations</p>
-				</div>
-				<div class="stat stat-2">
-					<p class="fs-700 fw-semibold margin-b-10">£15.1m</p>
-					<p class="fs-200 fw-semibold">Operating profit</p>
-				</div>
-				<div class="stat stat-3">
-					<p class="fs-700 fw-semibold margin-b-10">75%</p>
-					<p class="fs-200 fw-semibold">Employee retention</p>
-				</div>
-			</div>
-		</div>
-	</div>
+$stat_1_value = get_sub_field('data_map_stat_1_value');
+$stat_1_text = get_sub_field('data_map_stat_1_text');
+
+$stat_2_value = get_sub_field('data_map_stat_2_value');
+$stat_2_text = get_sub_field('data_map_stat_2_text');
+
+$stat_3_value = get_sub_field('data_map_stat_3_value');
+$stat_3_text = get_sub_field('data_map_stat_3_text');
+
+// Extract image URLs with fallbacks
+$bg_image_url = $bg_image ? $bg_image['url'] : get_template_directory_uri() . '/images/data-map-bg.jpg';
+$map_image_url = $map_image ? $map_image['url'] : get_template_directory_uri() . '/images/data-map.png';
+?>
+
+<div class="data-map padding-t-b-100 theme-dark" style="background-image:url('<?php echo esc_url($bg_image_url); ?>')">
+    <div class="cont-m">
+        <div class="stats">
+            <div class="stat stat-1">
+                <img src="<?php echo esc_url($map_image_url); ?>" />
+                <?php if ($stat_1_value): ?>
+                    <p class="fs-700 fw-semibold margin-b-10"><?php echo esc_html($stat_1_value); ?></p>
+                <?php endif; ?>
+                <?php if ($stat_1_text): ?>
+                    <p class="fs-200 fw-semibold"><?php echo esc_html($stat_1_text); ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="stat stat-2">
+                <?php if ($stat_2_value): ?>
+                    <p class="fs-700 fw-semibold margin-b-10"><?php echo esc_html($stat_2_value); ?></p>
+                <?php endif; ?>
+                <?php if ($stat_2_text): ?>
+                    <p class="fs-200 fw-semibold"><?php echo esc_html($stat_2_text); ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="stat stat-3">
+                <?php if ($stat_3_value): ?>
+                    <p class="fs-700 fw-semibold margin-b-10"><?php echo esc_html($stat_3_value); ?></p>
+                <?php endif; ?>
+                <?php if ($stat_3_text): ?>
+                    <p class="fs-200 fw-semibold"><?php echo esc_html($stat_3_text); ?></p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 	<style type="text/css">
 		.stat {
@@ -26,7 +56,7 @@
 		}
 		.stats {
 			display:flex;
-			width:50%;
+			width:min(700px, 100%);
 			gap:20px;
 			flex-wrap:wrap
 		}
