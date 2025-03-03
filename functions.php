1b0f8b18bfa36a2f2b3b3b2cc32eab3fb6e5925a
Steven Park <info@stevenpark.co.uk>
2025-03-03 22:07:01 +0000

    Lint and Tidy CSS and JS

--- functions.php
+++ functions.php
@@ -108,7 +108,6 @@ function zotefoams_enqueue_assets() {
     // Swiper CSS & JS
     wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@latest/swiper-bundle.min.css');
     wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), null, true);
-    wp_enqueue_script('swiper-custom', get_template_directory_uri() . '/js/swiper-custom.js', array('swiper-js'), null, true);
 
     // Animate.css
     wp_enqueue_style('animate-css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
@@ -117,13 +116,13 @@ function zotefoams_enqueue_assets() {
     // Navigation Script
     wp_enqueue_script('zotefoams-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-    // Components Assets
+    // Component CSS & JS
     wp_enqueue_style('zotefoams-components-style', get_template_directory_uri() . '/css/components.css', array(), _S_VERSION);
     wp_enqueue_script('zotefoams-components-js', get_template_directory_uri() . '/js/components.js', array(), _S_VERSION, true);
     wp_enqueue_style('zotefoams-components-carousels-style', get_template_directory_uri() . '/css/components-carousels.css', array(), _S_VERSION);
     wp_enqueue_script('zotefoams-components-carousels-js', get_template_directory_uri() . '/js/components-carousels.js', array(), _S_VERSION, true);
 
-    // Stevens (Temp) Assets
+    // Stevens (WIP) Assets
     wp_enqueue_style('zotefoams-sp-style', get_template_directory_uri() . '/css/sp.css', array(), _S_VERSION);
     wp_enqueue_script('zotefoams-sp-js', get_template_directory_uri() . '/js/sp.js', array(), _S_VERSION, true);
 
