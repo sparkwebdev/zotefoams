ab6c72dd17e28d53d19e8ce6f5660b43668fa74b
Steven Park <info@stevenpark.co.uk>
2025-03-06 16:20:20 +0000

    KH - Technical LP tweak

--- template-parts/content-knowledge-hub-section-technical.php
+++ template-parts/content-knowledge-hub-section-technical.php
@@ -1,6 +1,6 @@
 <?php
-// Define your brands array. Currently unused
-// $brands = array('');
+// Define your brands array. It can now be empty.
+$brands = array();
 
 // Query the child pages of the current page using the 'knowledge-hub' post type.
 $child_pages = get_pages(array(
@@ -11,10 +11,9 @@ $child_pages = get_pages(array(
 ));
 ?>
 
-<?php if (!empty($brands)) : ?>
 <nav class="cont-m margin-t-70 margin-b-100">
     <div data-component="section-list">
-        <?php if (count($brands) > 1) : ?>
+        <?php if (!empty($brands) && count($brands) > 1) : ?>
             <div class="file-list__dropdown">
                 <button id="filter-toggle" class="file-list__dropdown-button hl arrow">
                     Filter
@@ -33,34 +32,48 @@ $child_pages = get_pages(array(
 
         <div class="section-list">
             <div class="articles articles--grid-alt margin-t-30">
-                <?php 
-                // Loop over each brand.
-                foreach ($brands as $brand) : ?>
-                    <?php 
-                    // For each child page, output an article.
-                    foreach ($child_pages as $child) : 
-                        $child_title = $child->post_title;
-                        $brand_id = zotefoams_get_page_id_by_title($brand);
-                        $child_url = esc_url(get_the_permalink($child->ID));
-                        if (!empty($brand)) {
-                            $child_url .= '?brand=' . $brand_id;
-                        }
-                        ?>
-                        <article id="post-<?php echo esc_attr($child->ID); ?>" <?php post_class('section-list__item', $child->ID); ?> data-brand="<?php echo esc_attr($brand); ?>" data-gallery-label="<?php echo esc_attr($brand); ?>" data-clickable-url="<?php echo $child_url;?>">
-                            <div class="articles__content padding-30">
-                                <div class="tags margin-b-20">
-                                    <span class="tag"><?php echo esc_html($brand); ?></span>
+                <?php if (!empty($brands)) : ?>
+                    <?php
+                    // If there are brands, loop through each brand and then each child page.
+                    foreach ($brands as $brand) :
+                        foreach ($child_pages as $child) :
+                            $child_title = $child->post_title;
+                            $brand_id = zotefoams_get_page_id_by_title($brand);
+                            $child_url = esc_url(get_the_permalink($child->ID)) . '?brand=' . $brand_id;
+                    ?>
+                            <article id="post-<?php echo esc_attr($child->ID); ?>" <?php post_class('section-list__item', $child->ID); ?> data-brand="<?php echo esc_attr($brand); ?>" data-gallery-label="<?php echo esc_attr($brand); ?>" data-clickable-url="<?php echo $child_url; ?>">
+                                <div class="articles__content padding-30">
+                                    <div class="tags margin-b-20">
+                                        <span class="tag"><?php echo esc_html($brand); ?></span>
+                                    </div>
+                                    <h3 class="fs-400 fw-semibold margin-b-20"><?php echo esc_html($child_title); ?></h3>
+                                    <p class="articles__cta">
+                                        <a href="<?php echo $child_url; ?>" class="hl arrow read-more">View Information Sheets</a>
+                                    </p>
                                 </div>
+                            </article>
+                    <?php
+                        endforeach;
+                    endforeach;
+                    ?>
+                <?php else : ?>
+                    <?php
+                    // If there are no brands, simply loop through child pages without any brand filter or tags.
+                    foreach ($child_pages as $child) :
+                        $child_title = $child->post_title;
+                        $child_url = esc_url(get_the_permalink($child->ID));
+                    ?>
+                        <article id="post-<?php echo esc_attr($child->ID); ?>" <?php post_class('section-list__item', $child->ID); ?> data-clickable-url="<?php echo $child_url; ?>">
+                            <div class="articles__content padding-30">
                                 <h3 class="fs-400 fw-semibold margin-b-20"><?php echo esc_html($child_title); ?></h3>
                                 <p class="articles__cta">
-                                    <a href="<?php echo $child_url;?>" class="hl arrow read-more">View Information Sheets</a>
+                                    <a href="<?php echo $child_url; ?>" class="hl arrow read-more">View Information Sheets</a>
                                 </p>
                             </div>
                         </article>
                     <?php endforeach; ?>
-                <?php endforeach; ?>
+                <?php endif; ?>
             </div>
         </div>
     </div>
 </nav>
-<?php endif; ?>
