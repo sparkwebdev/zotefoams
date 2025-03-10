51afdf7718ed7683e455dad0f09ed80946eed7d9
Manbuild <davidbenpark@gmail.com>
2025-03-10 15:23:10 +0000

    Images added to CMS

--- template-parts/components/waste-hierarchy.php
+++ template-parts/components/waste-hierarchy.php
@@ -3,10 +3,10 @@
 	<div class="waste-hierarchy cont-m">
 		<div class="waste-hierarchy-intro text-center">
 			<h3 class="fs-600 margin-b-20">The Hierarchy of Waste</h3>
-			<p class="fs-300 grey-text margin-b-70">Zotefoams put in place a Group-wide Waste Hierarchy Framework to support decision-making on waste management.</p>
+			<p class="fs-300 grey-text margin-b-70">Zotefoams has put in place a Group-wide Waste Hierarchy Framework to support decision-making on waste management.</p>
 		</div>
 
-		<img src="<?php echo get_template_directory_uri(); ?>/images/waste-hierarchy.png" />
+		<img src="<?php echo get_template_directory_uri(); ?>/images/waste-hierarchy.svg" />
 	</div>
 
 </div>
