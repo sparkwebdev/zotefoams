369b5f622bf55ba7ef27fe50160ace217a571a5c
Steven Park <info@stevenpark.co.uk>
2025-04-02 09:38:25 +0100

    Small template tidy; Better fallback font; Remove unused title-strip

--- inc/admin.php
+++ inc/admin.php
@@ -11,64 +11,20 @@ function my_acf_admin_head()
 {
     ?>
     <style type="text/css">
-
-		.acf-postbox>.inside {
-			padding:10px 10px 10px 10px !important
-		}
-		.edit-post-meta-boxes-area #poststuff .stuffbox>h3, .edit-post-meta-boxes-area #poststuff h2.hndle, .edit-post-meta-boxes-area #poststuff h3.hndle {
-			border-bottom: 0;
-			font-size: 21px;
-			font-family: arial;
-		}
-		.edit-post-meta-boxes-area .postbox>.inside {
-			background: #f2f2f2;
-		}
-		.hndle:hover {
-			background: #fff !important;
-		}
-		.postbox-container .meta-box-sortables {
-			margin-bottom: 100px;
-		}
-		.postbox.acf-postbox {
-			margin: 2%;
-    		border: 1px solid #ccc;
-		}
 		.postbox-header {
 			background:#1d2327;
 		}
-		.hndle:hover {
-			background:#1d2327 !important;
+		.postbox-header > * {
+			color:#fff;
 		}
-		#poststuff h2,
-		.acf-admin-page #poststuff .postbox-header h2, 
-		.acf-admin-page #poststuff .postbox-header h3 {
-			color:#fff !important;
-		}
-		.acf-table {border-collapse: collapse; }
 		.acf-table > tbody > tr {
-			border-top: 2px solid black;
-		}
-		.acf-admin-page .postbox .postbox-header, .acf-admin-page .postbox .title, .acf-admin-page .acf-box .postbox-header, .acf-admin-page .acf-box .title {
-			min-height: 3rem;
+			border-top: 3px solid #8c8f94;
 		}
     </style>
- 
-    <script type="text/javascript">
-    (function($){
- 
-        /* ... */
- 
-    })(jQuery);
-    </script>
     <?php
 }
  
 add_action('acf/input/admin_head', 'my_acf_admin_head');
-
-/**
- * Hide Gutenberg Block editor.
- */
-// add_filter('use_block_editor_for_post', '__return_false');
 
 /**
  * Disable Application Passwords.
