6fde1159c521a2f71579985f47a11c54a924cdfd
Steven Park <info@stevenpark.co.uk>
2025-05-23 22:25:28 +0100

    Split Video One, Split Video Two component tidy

--- template-parts/components/split_video_one.php
+++ template-parts/components/split_video_one.php
@@ -7,75 +7,44 @@ $extra_text  = get_sub_field('split_video_one_extra_text');
 $link        = get_sub_field('split_video_one_link');
 
 $image_url = $image ? $image['sizes']['large'] : get_template_directory_uri() . '/images/placeholder.png';
+$theme_class = is_page('Sustainability') ? 'theme-light' : 'theme-none';
 ?>
 
-<?php if (is_page('Sustainability')) : ?>
-
-	<div class="split-grey half-half cont-m padding-b-100 theme-none">
-		<div class="half image-cover" style="background-image:url('<?php echo esc_url($image_url); ?>');"></div>
-
-		<div class="half light-grey-bg padding-50">
-			<div class="top">
-				<?php if ($title) : ?>
-					<p class="fs-200 fw-regular margin-b-10"><?php echo esc_html($title); ?></p>
-				<?php endif; ?>
-
-				<?php if ($text) : ?>
-					<div class="fs-400 fw-semibold margin-b-10"><?php echo wp_kses_post($text); ?></div>
-				<?php endif; ?>
-
-				<?php if ($extra_text) : ?>
-					<span class="fs-300 text-margin margin-b-30"><?php echo wp_kses_post($extra_text); ?></span>
-				<?php endif; ?>
-			</div>
-
-			<div class="bottom">
-				<?php if ($link) : ?>
-					<a href="<?php echo esc_url($link['url']); ?>" class="hl arrow" target="<?php echo esc_attr($link['target']); ?>">
-						<?php echo esc_html($link['title']); ?>
-					</a>
-				<?php endif; ?>
-			</div>
-		</div>
+<div class="split-video-one cont-m padding-t-b-100 <?php echo $theme_class; ?>">
+	<div class="video-container image-cover" style="background-image:url('<?php echo esc_url($image_url); ?>');">
+		<?php if ($video_url) : ?>
+			<button type="button" class="video-trigger" data-modal-trigger="video" data-video-url="<?php echo esc_url($video_url); ?>" aria-label="<?php esc_attr_e('Play Video', 'zotefoams'); ?>">
+				<img src="<?php echo esc_url(get_template_directory_uri() . '/images/youtube-play.svg'); ?>" alt="" />
+			</button>
+		<?php endif; ?>
 	</div>
 
-<?php else : ?>
+	<div <?php echo is_page('Sustainability') ? 'class="light-grey-bg padding-50"' : ''; ?>>
+		<?php if ($title) : ?>
+			<p class="fs-200 fw-regular margin-b-30"><?php echo esc_html($title); ?></p>
+		<?php endif; ?>
 
-	<div class="split-video-one half-half cont-m padding-t-b-100 theme-none">
-		<div class="half video-container image-cover" style="background-image:url('<?php echo esc_url($image_url); ?>');">
-			<?php if ($video_url) : ?>
-				<button type="button" class="video-trigger" data-modal-trigger="video" data-video-url="<?php echo esc_url($video_url); ?>" aria-label="<?php esc_attr_e('Play Video', 'zotefoams'); ?>">
-					<img src="<?php echo esc_url(get_template_directory_uri() . '/images/youtube-play.svg'); ?>" alt="" />
-				</button>
-			<?php endif; ?>
-		</div>
+		<?php if ($text) : ?>
+			<div class="fs-500 fw-semibold margin-b-40"><?php echo wp_kses_post($text); ?></div>
+		<?php endif; ?>
 
-		<div class="half">
-			<?php if ($title) : ?>
-				<p class="fs-200 fw-regular margin-b-30"><?php echo esc_html($title); ?></p>
-			<?php endif; ?>
+		<?php if ($extra_text) : ?>
+			<div class="fs-300 text-margin margin-b-50">
+				<?php echo wp_kses_post($extra_text); ?>
+			</div>
+		<?php endif; ?>
 
-			<?php if ($text) : ?>
-				<div class="fs-500 fw-semibold margin-b-40"><?php echo wp_kses_post($text); ?></div>
-			<?php endif; ?>
-
-			<?php if ($extra_text) : ?>
-				<div class="fs-300 text-margin margin-b-50">
-					<?php echo wp_kses_post($extra_text); ?>
-				</div>
-			<?php endif; ?>
-
-			<?php if ($link) : ?>
-				<div class="margin-b-30">
-					<a href="<?php echo esc_url($link['url']); ?>" class="hl arrow" target="<?php echo esc_attr($link['target']); ?>">
-						<?php echo esc_html($link['title']); ?>
-					</a>
-				</div>
-			<?php endif; ?>
-		</div>
+		<?php if ($link) : ?>
+			<div class="margin-b-30">
+				<a href="<?php echo esc_url($link['url']); ?>" class="hl arrow" target="<?php echo esc_attr($link['target']); ?>">
+					<?php echo esc_html($link['title']); ?>
+				</a>
+			</div>
+		<?php endif; ?>
 	</div>
+</div>
 
-<?php endif; ?>
+
 
 <!-- Video Overlay Structure -->
 <?php require_video_overlay(); ?>
--- template-parts/components/split_video_two.php
+++ template-parts/components/split_video_two.php
@@ -7,28 +7,25 @@ $text       = get_sub_field('split_video_two_text');
 $image_url = $image ? $image['sizes']['large'] : get_template_directory_uri() . '/images/placeholder.png';
 ?>
 
-<!-- Half & Half image + text block -->
 <div class="split-video-two black-bg white-text theme-dark">
-    <div class="split-video-two-inner half-half">
-        <div class="half padding-t-b-100 text-content">
-            <?php if ($title) : ?>
-                <p class="fs-600 fw-regular margin-b-30"><?php echo esc_html($title); ?></p>
-            <?php endif; ?>
+    <div class="padding-t-b-100">
+        <?php if ($title) : ?>
+            <p class="fs-600 fw-regular margin-b-30"><?php echo esc_html($title); ?></p>
+        <?php endif; ?>
 
-            <?php if ($text) : ?>
-                <span class="fs-300 text-margin">
-                    <?php echo wp_kses_post($text); ?>
-                </span>
-            <?php endif; ?>
-        </div>
+        <?php if ($text) : ?>
+            <span class="fs-300 text-margin">
+                <?php echo wp_kses_post($text); ?>
+            </span>
+        <?php endif; ?>
+    </div>
 
-        <div class="half video-container image-cover" style="background-image:url('<?php echo esc_url($image_url); ?>');">
-            <?php if ($video_url) : ?>
-                <button type="button" class="video-trigger" data-modal-trigger="video" data-video-url="<?php echo esc_url($video_url); ?>" aria-label="<?php esc_attr_e('Play Video', 'zotefoams'); ?>">
-					<img src="<?php echo esc_url(get_template_directory_uri() . '/images/youtube-play.svg'); ?>" alt="" />
-				</button>
-            <?php endif; ?>
-        </div>
+    <div class="video-container image-cover" style="background-image:url('<?php echo esc_url($image_url); ?>');">
+        <?php if ($video_url) : ?>
+            <button type="button" class="video-trigger" data-modal-trigger="video" data-video-url="<?php echo esc_url($video_url); ?>" aria-label="<?php esc_attr_e('Play Video', 'zotefoams'); ?>">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/images/youtube-play.svg'); ?>" alt="" />
+            </button>
+        <?php endif; ?>
     </div>
 </div>
 
