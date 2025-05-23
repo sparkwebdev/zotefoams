8e484bfdfec3ef719ad64a40b3f7a738588fcc34
Steven Park <info@stevenpark.co.uk>
2025-05-23 22:47:40 +0100

    Related Links Box component tidy

--- template-parts/components/related_links_box.php
+++ template-parts/components/related_links_box.php
@@ -3,34 +3,36 @@ $title = get_sub_field( 'related_links_box_title' );
 $links = get_sub_field( 'related_links_box_links' );
 ?>
 
-<aside class="cont-xs related-links-box padding-t-70 margin-b-100 theme-none">
-	<?php if ( ! empty( $title ) ) : ?>
-		<h3 class="related-links-box__title"><?php echo wp_kses_post( $title ); ?></h3>
-	<?php endif; ?>
+<aside class="related-links-box margin-t-0 padding-t-b-100 theme-none">
+	<div class="cont-xs">
+		<?php if ( ! empty( $title ) ) : ?>
+			<h3 class="related-links-box__title"><?php echo wp_kses_post( $title ); ?></h3>
+		<?php endif; ?>
 
-	<?php if ( $links ) : ?>
-		<nav class="related-links-box__links">
-			<ul>
-				<?php foreach ( $links as $link ) : 
-					$link_data = $link['related_links_box_link'] ?? null;
+		<?php if ( $links ) : ?>
+			<nav class="related-links-box__links">
+				<ul>
+					<?php foreach ( $links as $link ) : 
+						$link_data = $link['related_links_box_link'] ?? null;
 
-					if ( $link_data ) :
-						$link_url    = $link_data['url'] ?? '';
-						$link_title  = $link_data['title'] ?? '';
-						$link_target = $link_data['target'] ?? '_self';
-						$is_file     = $link_url && wp_check_filetype( $link_url )['ext'];
-						$link_class  = $is_file ? 'related-links-box__link-download' : 'related-links-box__link-arrow';
-				?>
-					<li>
-						<a 
-							class="related-links-box__link <?php echo esc_attr( $link_class ); ?>"
-							href="<?php echo esc_url( $link_url ); ?>"
-							target="<?php echo esc_attr( $link_target ); ?>">
-							<?php echo esc_html( $link_title ); ?>
-						</a>
-					</li>
-				<?php endif; endforeach; ?>
-			</ul>
-		</nav>
-	<?php endif; ?>
+						if ( $link_data ) :
+							$link_url    = $link_data['url'] ?? '';
+							$link_title  = $link_data['title'] ?? '';
+							$link_target = $link_data['target'] ?? '_self';
+							$is_file     = $link_url && wp_check_filetype( $link_url )['ext'];
+							$link_class  = $is_file ? 'related-links-box__link-download' : 'related-links-box__link-arrow';
+					?>
+						<li>
+							<a 
+								class="related-links-box__link <?php echo esc_attr( $link_class ); ?>"
+								href="<?php echo esc_url( $link_url ); ?>"
+								target="<?php echo esc_attr( $link_target ); ?>">
+								<?php echo esc_html( $link_title ); ?>
+							</a>
+						</li>
+					<?php endif; endforeach; ?>
+				</ul>
+			</nav>
+		<?php endif; ?>
+	</div>
 </aside>
