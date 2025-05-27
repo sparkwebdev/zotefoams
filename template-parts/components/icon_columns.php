ae76301be1dfd4c7ec51ae6e439781389cb8e2e3
Steven Park <info@stevenpark.co.uk>
2025-05-27 16:37:14 +0100

    Refactor Icon Columns to be more flexible and cover various layouts

--- template-parts/components/icon_columns.php
+++ template-parts/components/icon_columns.php
@@ -4,28 +4,44 @@ $intro    = get_sub_field('icon_columns_intro');
 $columns  = get_sub_field('icon_columns_columns');
 
 $is_sustainability = is_page('Sustainability');
-$wrapper_class = $is_sustainability ? 'sustainability-icons black-text' : 'icon-columns light-grey-bg black-text';
-$theme_class = $is_sustainability ? 'theme-none' : 'theme-light';
-$columns_wrapper_class = $is_sustainability ? 'sustainability-icons-inner' : 'icon-columns__inner';
+$wrapper_class = 'icon-columns';
+
+if (!empty($columns)) {
+    $count = count($columns);
+	$wrapper_class .= ($overline || $intro) ? ' theme-none' : ' light-grey-bg theme-light';
+    $wrapper_class .= ($count == 2 || $count == 4) ? ' icon-columns--half' : '';
+	// $wrapper_class .= ($count === 5) ? ' icon-columns--centered' : '';
+
+    // Check if ALL columns have empty 'text' field
+    $all_columns_without_text = true;
+    foreach ($columns as $column) {
+        if (!empty($column['icon_columns_text'])) {
+            $all_columns_without_text = false;
+            break;
+        }
+    }
+
+    if ($all_columns_without_text) {
+        $wrapper_class .= ' icon-columns--centered';
+    }
+}
 ?>
 
-<div class="<?php echo esc_attr($wrapper_class); ?> padding-t-b-100 <?php echo esc_attr($theme_class); ?>">
+<div class="<?php echo esc_attr($wrapper_class); ?> padding-t-b-100">
 
-	<?php if ($is_sustainability): ?>
-	<div class="sustainability-intro grey-text cont-m">
-		<div class="sustainability-intro-inner">
-			<?php if ($overline): ?>
-				<p class="margin-b-20"><?php echo wp_kses_post($overline); ?></p>
-			<?php endif; ?>
-			<?php if ($intro): ?>
-				<h3 class="fs-600 margin-b-70 grey-text"><?php echo wp_kses_post($intro); ?></h3>
-			<?php endif; ?>
-		</div>
+	<?php if ($overline || $intro): ?>
+	<div class="text-block cont-m margin-b-70">
+		<?php if ($overline): ?>
+			<p class="margin-b-20"><?php echo wp_kses_post($overline); ?></p>
+		<?php endif; ?>
+		<?php if ($intro): ?>
+			<h3 class="fs-600 grey-text fw-semibold"><?php echo wp_kses_post($intro); ?></h3>
+		<?php endif; ?>
 	</div>
-	<?php endif; ?>
-
-	<div class="<?php echo esc_attr($columns_wrapper_class); ?> cont-m">
-		<?php if ($columns): ?>
+    <?php endif; ?>
+		
+	<?php if ($columns): ?>
+		<div class="icon-columns__items cont-m">
 			<?php foreach ($columns as $column):
 				$icon  = $column['icon_columns_icon'];
 				$title = $column['icon_columns_title'];
@@ -43,8 +59,8 @@ $columns_wrapper_class = $is_sustainability ? 'sustainability-icons-inner' : 'ic
 					<?php endif; ?>
 				</div>
 			<?php endforeach; ?>
-		<?php endif; ?>
-	</div>
+		</div>
+	<?php endif; ?>
 </div>
 
 <?php if ($is_sustainability): ?>
