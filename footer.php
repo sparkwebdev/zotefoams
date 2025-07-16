d5b90b31c42958341a864da789e3dee8143d909e
Steven Park <info@stevenpark.co.uk>
2025-07-16 11:17:16 +0100

    Match live

--- footer.php
+++ footer.php
@@ -81,7 +81,7 @@
 
 			<div class="footer-newsletter">
 				<?php
-				get_template_part('template-parts/parts/mailchimp');
+					get_template_part('template-parts/parts/mailchimp');
 				?>
 			</div>
 
@@ -104,40 +104,7 @@
 
 <?php wp_footer(); ?>
 
-<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
-<script type="text/javascript">
-	(function($) {
-		window.fnames = new Array();
-		window.ftypes = new Array();
-		fnames[0] = 'EMAIL';
-		ftypes[0] = 'email';
-		fnames[1] = 'FNAME';
-		ftypes[1] = 'text';
-		fnames[2] = 'LNAME';
-		ftypes[2] = 'text';
-		fnames[3] = 'ADDRESS';
-		ftypes[3] = 'address';
-		fnames[4] = 'PHONE';
-		ftypes[4] = 'phone';
-		fnames[5] = 'MMERGE5';
-		ftypes[5] = 'text';
-		fnames[6] = 'MMERGE6';
-		ftypes[6] = 'text';
-		fnames[7] = 'MMERGE7';
-		ftypes[7] = 'text';
-		fnames[8] = 'MMERGE8';
-		ftypes[8] = 'number';
-		fnames[9] = 'MMERGE9';
-		ftypes[9] = 'text';
-		fnames[10] = 'MMERGE10';
-		ftypes[10] = 'text';
-		fnames[11] = 'MMERGE11';
-		ftypes[11] = 'text';
-		fnames[12] = 'MMERGE12';
-		ftypes[12] = 'text';
-	}(jQuery));
-	var $mcj = jQuery.noConflict(true);
-</script>
+<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='MMERGE5';ftypes[5]='text';fnames[6]='MMERGE6';ftypes[6]='text';fnames[7]='MMERGE7';ftypes[7]='text';fnames[8]='MMERGE8';ftypes[8]='number';fnames[9]='MMERGE9';ftypes[9]='text';fnames[10]='MMERGE10';ftypes[10]='text';fnames[11]='MMERGE11';ftypes[11]='text';fnames[12]='MMERGE12';ftypes[12]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 
 </body>
 
