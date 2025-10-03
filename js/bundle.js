6bd6567012ef5fc3e6979f560c70ef5881138bf8
Steven Park <info@stevenpark.co.uk>
2025-10-03 11:03:15 +0100

    Match live line endings

--- js/bundle.js
+++ js/bundle.js
@@ -290,14 +290,8 @@
 	    if (url) {
 	      const matchingChild = article.querySelector('[href="' + url + '"]');
 	      if (matchingChild) {
-	        article.addEventListener('click', function (event) {
-	          if (event.metaKey || event.ctrlKey) {
-	            window.open(url, '_blank');
-	          } else if (event.shiftKey) {
-	            window.open(url);
-	          } else {
-	            matchingChild.click();
-	          }
+	        article.addEventListener('click', function () {
+	          matchingChild.click();
 	        });
 	      }
 	    }
